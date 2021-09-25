ff741b2efe6cf8907d46367156d0c50d2e00d638
Chi <cmyip99@gmail.com>
2021-09-25 20:38:29 +1000

    Copying files

---
+++ src/app/database.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DatabaseService {
+
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get("/movies");
+  }
+
+  getMovie(id: string) {
+    let url = "/movies/" + id;
+    return this.http.get(url);
+  }
+
+  deleteMovie(id: string) {
+    let url = "/movies/" + id;
+    return this.http.delete(url, httpOptions);
+  }
+
+  deleteMoviesByYear(aYear1: number, aYear2: number){
+    let url = '/movies/' + aYear1 + '/' + aYear2;
+    return this.http.delete(url, httpOptions);
+  }
+
+  deleteMovieByTitle(title: string) {
+    let url = '/movies/' + title + '/title/title';
+    return this.http.delete(url, httpOptions)
+  }
+
+  addActorToMovie(data: any, id: string){
+    let url = '/movies/' + id + '/actors';
+    return this.http.post(url, data, httpOptions)
+  }
+
+  addMovieToActor(data: any, id: string){
+    let url = '/actors/' + id + '/movies'
+    return this.http.post(url, data, httpOptions)
+  }
+
+  getActors() {
+    return this.http.get("/actors");
+  }
+
+  getActor(id: string) {
+    let url = "/actors/" + id;
+    return this.http.get(url);
+  }
+
+  createActor(data: any) {
+    return this.http.post("/actors", data, httpOptions);
+  }
+
+  createMovie(data: any) {
+    return this.http.post("/movies", data, httpOptions);
+  }
+
+  updateActor(id: string, data: any) {
+    let url = "/actors/" + id;
+    return this.http.put(url, data, httpOptions);
+  }
+
+  deleteActor(id: string) {
+    let url = "/actors/" + id;
+    return this.http.delete(url, httpOptions);
+  }
+}
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from "../database.service";
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+  moviesDB: any[] = [];
+  section = 1;
+  movieId: string = "";
+  title: string = "";
+  year: number = 0;
+  aYear1: number = 0;
+  aYear2: number = 0;
+  actors: Object[] = [];
+  actorsDB1: any[] = [];
+  actorl: string = '';
+  x: any;
+  i: any;
+  j: any;
+  aTitle: string = "";
+
+  constructor(private dbService: DatabaseService) { }
+
+  ngOnInit() {
+    this.onGetMovies();
+  }
+
+  // Get all movies
+  onGetMovies() {
+    this.dbService.getMovies().subscribe((data: any) => {
+      this.moviesDB = data;
+    });
+  }
+
+  // Create a new movie, POST request
+  onSaveMovie() {
+    let obj = { title: this.title, year: this.year};
+    this.dbService.createMovie(obj).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  // Delete Movie
+  onDeleteMovie(item: any) {
+    this.dbService.deleteMovie(item._id).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+
+  // Add actor to movie
+  onAddActorToMovie() {
+    let actorObj = { id: this.x._id };
+    let movieObj = { id: this.i._id };
+    this.dbService.addActorToMovie(actorObj, this.i._id).subscribe(result => {
+      this.dbService.addMovieToActor(movieObj, this.x._id).subscribe(result => {
+        this.onGetMovies();
+      });
+      this.onGetMovies();
+    })
+  }
+
+  onActiveActors(){
+    this.dbService.getActors().subscribe((data: any)=>{
+      this.actorsDB1 = data;
+      this.onGetMovies();
+    })
+  }
+
+  onDeleteMovieByTitle(title: string) {
+    this.dbService.deleteMovieByTitle(title).subscribe(result => {
+      this.onGetMovies();
+    })
+  }
+
+  // Delete Movies by Year
+  onDeleteMoviesByYear(aYear1: number, aYear2: number) {
+    this.dbService.deleteMoviesByYear(aYear1, aYear2).subscribe(result => {
+      this.onGetMovies();
+    });
+  }
+
+  changeSection(sectionId: number) {
+    this.section = sectionId;
+    this.resetValues();
+    this.onActiveActors();
+  }
+
+  resetValues() {
+    this.movieId = "";
+    this.title = "";
+    this.year = 0;
+    this.actors = [];
+  }
+}
